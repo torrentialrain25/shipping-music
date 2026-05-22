72e6935421708657c0485911ccf2010b85b1e222
TorrentialRain <torrentialrain25@gmail.com>
2026-05-22 18:07:09 +0800

    switch font to Google Fonts CDN for faster loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/noto-serif-sc/400.css'
-import '@fontsource/noto-serif-sc/700.css'
 import './index.css'
 import App from './App.jsx'
 
